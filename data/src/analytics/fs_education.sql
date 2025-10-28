f23f8e38603948114588a2dfe9d0ec6ef4f6b8e7
Guilherme Alves <guilherme.alves1amo@gmail.com>
2025-10-28 11:30:56 -0300

    feat: education complete

--- data/src/analytics/fs_education.sql
+++ data/src/analytics/fs_education.sql
@@ -28,9 +28,8 @@ ON t1.descSlugCurso = t2.descSlugCurso
 ),
 tb_pct_cursos_pivot AS (
     SELECT idUsuario,
-        SUM(CASE WHEN pctCursoCompleto = 1 THEN 1 ELSE 0 END) AS qtdeCursoCompletos,
+                                 SUM(CASE WHEN pctCursoCompleto = 1 THEN 1 ELSE 0 END) AS qtdeCursoCompletos,
         SUM(CASE WHEN pctCursoCompleto > 0 AND pctCursoCompleto < 1 THEN 1 ELSE 0 END) AS qtdeCursoImcompletos,
-
         SUM(CASE WHEN descSlugCurso = 'python-2025' THEN pctCursoCompleto ELSE 0 END ) AS python2025,
         SUM(CASE WHEN descSlugCurso = 'github-2025' THEN pctCursoCompleto ELSE 0 END ) AS github2025,
         SUM(CASE WHEN descSlugCurso = 'estatistica-2025' THEN pctCursoCompleto ELSE 0 END ) AS estatistica2025,
@@ -117,9 +116,68 @@ SELECT idUsuario,
 FROM tb_atividade
 
 GROUP BY idUsuario
+),
+tb_join AS (
+    SELECT  t3.idTMWCliente,
+            t2.ultimaInteracao,
+            t1.qtdeCursoCompletos,
+            t1.qtdeCursoImcompletos,
+            t1.python2025,
+            t1.github2025,
+            t1.estatistica2025,
+            t1.github2025,
+            t1.python2025,
+            t1.sql2025,
+            t1.github2025,
+            t1.github2025,
+            t1.github2025,
+            t1.python2025,
+            t1.github2025,
+            t1.python2025,
+            t1.python2025,
+            t1.github2025,
+            t1.python2025,
+            t1.github2025,
+            t1.pandas2025,
+            t1.python2025,
+            t1.sql2020,
+            t1.sql2020,
+            t1.github2025,
+            t1.python2025,
+            t1.python2025,
+            t1.sql2025,
+            t1.github2025,
+            t1.python2025,
+            t1.python2025,
+            t1.python2025,
+            t1.python2025,
+            t1.github2025,
+            t1.pandas2024,
+            t1.pandas2025,
+            t1.python2025,
+            t1.python2025,
+            t1.sql2020,
+            t1.estatistica2024,
+            t1.mlflow2025,
+            t1.pandas2025,
+            t1.estatistica2024,
+            t1.github2025,
+            t1.github2024,
+            t1.github2025,
+            t1.pandas2024,
+            t1.pandas2025,
+            t1.python2025,
+            t1.github2025,
+            t1.python2025,
+            t1.sql2025,
+            t1.pandas2025
+
+    FROM tb_pct_cursos_pivot AS t1
+    LEFT JOIN tb_ultima_atividade AS t2
+    ON t1.idUsuario = t2.idUsuario
+    INNER JOIN usuarios_tmw AS t3
+    ON t1.idUsuario = t3.idUsuario
 )
-SELECT t1.*,
-       t2.ultimaInteracao
-FROM tb_pct_cursos_pivot AS t1
-LEFT JOIN tb_ultima_atividade AS t2
-ON t1.idUsuario = t2.idUsuario
+SELECT DATE('2025-10-01','-1 day') AS detRef,
+        *
+FROM tb_join
